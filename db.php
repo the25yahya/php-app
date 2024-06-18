8bbf2256d878345549dcf36222498272f5428770
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-06-18 19:02:45 +0200

    mc

--- db.php
+++ db.php
@@ -1,38 +1,12 @@
 <?php
-// Function to establish database connection
-function connectToDatabase() {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "php_app";
+$host="localhost";
+$db_name="php_app";
+$db_user= "root";
+$db_pass= "";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    return $conn;
+try{
+$pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
 }
 
-// Function to fetch some data from the database
-function getSomeDataFromDatabase() {
-    $conn = connectToDatabase();
-
-    $sql = "SELECT * FROM myTable";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "Name: " . $row["name"]. "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
-
-    $conn->close();
-}
 ?>
